866d4a8215199c5d67ebc8bc1f340654423ff916
FredyArroyave12 <43706273+FredyArroyave12@users.noreply.github.com>
2020-10-18 18:26:27 -0500

    Update time.c

--- ejercicios_refuerzo/time.c
+++ ejercicios_refuerzo/time.c
@@ -11,26 +11,26 @@ int main(int argc, char *argv[]){
 	
 	
 	struct timeval startingTime; 
-    struct timeval endingTime;
+    	struct timeval endingTime;
 	gettimeofday(&startingTime, NULL);
-    int cp = fork(); 
+  	  int cp = fork(); 
     
 	if (argc == 1)
         exit(1);
     
-    else{
+	else{
         
 		if (cp < 0){ 
             
-			fprintf(stderr, "fork failed\n");
+		fprintf(stderr, "fork failed\n");
             exit(1);
         }
         
 		else if (cp== 0){
             
-			char *b[argc]; 
+		char *b[argc]; 
 
-			for (int i = 0; i < argc - 1; i++) 
+		for (int i = 0; i < argc - 1; i++) 
             	b[i] = argv[i + 1]; 
             
            	b[argc - 1] = NULL; 
@@ -38,9 +38,9 @@ int main(int argc, char *argv[]){
         }
         else{ 
             
-			wait(NULL);
-            gettimeofday(&endingTime, NULL);
-            printf("elapsed time: %ld\n",(endingTime.tv_usec - startingTime.tv_usec)); 
+	wait(NULL);
+        gettimeofday(&endingTime, NULL);
+        printf("elapsed time: %ld\n",(endingTime.tv_usec - startingTime.tv_usec)); 
         }
     }
     return 0;
