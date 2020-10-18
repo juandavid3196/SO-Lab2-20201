18484c788dab8c13b5b57df99612e4d09ad63952
FredyArroyave12 <FredyArroyave@udea.edu.co>
2020-10-18 18:17:00 -0500

    ...

--- ejercicios_refuerzo/time.c
+++ ejercicios_refuerzo/time.c
@@ -10,38 +10,38 @@
 int main(int argc, char *argv[]){
 	
 	
-	struct timeval startingTime; // tiempo de inicio
-    struct timeval endingTime;// tiempo de finalizacion
-	gettimeofday(&startingTime, NULL);//obtiene la hora del reloj del sistema
-    int cp = fork(); // clonar proceso
+	struct timeval startingTime; 
+    struct timeval endingTime;
+	gettimeofday(&startingTime, NULL);
+    int cp = fork(); 
     
-	if (argc == 1) // si solo hay un atributo -- salir
+	if (argc == 1)
         exit(1);
     
     else{
         
-		if (cp < 0){ // si el fork retorna valor<0 -- salir
+		if (cp < 0){ 
             
 			fprintf(stderr, "fork failed\n");
             exit(1);
         }
         
-		else if (cp== 0){// proceso hijo= para ejecutar comando 
+		else if (cp== 0){
             
-			char *b[argc]; // array con espacio del total de atributos
+			char *b[argc]; 
 
-			for (int i = 0; i < argc - 1; i++) // argc -1 porque el primero no cuenta
-            	b[i] = argv[i + 1]; // pasar a el vector apartir del segundo argumento
+			for (int i = 0; i < argc - 1; i++) 
+            	b[i] = argv[i + 1]; 
             
-           	b[argc - 1] = NULL; //el vector  debe de terminar en puntero nulo por eso b[argc] 
-           	execvp(b[0], b);//ejecutar comando 
+           	b[argc - 1] = NULL; 
+           	execvp(b[0], b); 
         }
         else{ 
             
-			wait(NULL);// esperar ejecucion de comando
-            gettimeofday(&endingTime, NULL);// retornar tiempo actual del sistema
-            printf("elapsed time: %ld\n",(endingTime.tv_usec - startingTime.tv_usec)/10000); // tiempo total del proceso
+			wait(NULL);
+            gettimeofday(&endingTime, NULL);
+            printf("elapsed time: %ld\n",(endingTime.tv_usec - startingTime.tv_usec)/10000); 
         }
     }
-    return 0;//salir
+    return 0;
 }
